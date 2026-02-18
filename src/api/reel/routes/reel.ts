facf8e624b3927c50cf0a1eb7f4a5d4bcdd06b9c
thienanngo11122003 <thienanngo11122003@gmail.com>
2026-02-18 10:57:42 -0500

    feat: add versioned community routes for stories and reels

--- src/api/reel/routes/reel.ts
+++ src/api/reel/routes/reel.ts
@@ -2,6 +2,32 @@
  * reel router
  */
 
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::reel.reel');
+export default {
+  routes: [
+    {
+      method: 'GET',
+      path: '/v1/community/reels',
+      handler: 'reel.find',
+    },
+    {
+      method: 'POST',
+      path: '/v1/community/reels',
+      handler: 'reel.create',
+    },
+    {
+      method: 'GET',
+      path: '/v1/community/reels/:id',
+      handler: 'reel.findOne',
+    },
+    {
+      method: 'PUT',
+      path: '/v1/community/reels/:id',
+      handler: 'reel.update',
+    },
+    {
+      method: 'DELETE',
+      path: '/v1/community/reels/:id',
+      handler: 'reel.delete',
+    },
+  ],
+};
--- src/api/story/routes/story.ts
+++ src/api/story/routes/story.ts
@@ -2,6 +2,38 @@
  * story router
  */
 
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::story.story');
+export default {
+  routes: [
+    {
+      method: 'GET',
+      path: '/v1/community/stories',
+      handler: 'story.find',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'POST',
+      path: '/v1/community/stories',
+      handler: 'story.create',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'GET',
+      path: '/v1/community/stories/:id',
+      handler: 'story.findOne',
+    },
+    {
+      method: 'PUT',
+      path: '/v1/community/stories/:id',
+      handler: 'story.update',
+    },
+    {
+      method: 'DELETE',
+      path: '/v1/community/stories/:id',
+      handler: 'story.delete',
+    },
+  ],
+};
